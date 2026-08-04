5e4b020f48e95a7cd7c0b418bc3ac11351d2dc79
Hoai Nam <v.namnh141@vinsmartfuture.tech>
2026-08-04 12:22:47 +0700

    fix datepicker

--- components/ui/popover.tsx
+++ components/ui/popover.tsx
@@ -30,7 +30,7 @@ function PopoverContent({
         align={align}
         sideOffset={sideOffset}
         className={cn(
-          "glass z-50 w-auto rounded-3xl p-3 text-popover-foreground outline-none bg-white",
+          "glass z-50 w-auto rounded-3xl p-3 text-popover-foreground outline-none",
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
           className,
         )}
